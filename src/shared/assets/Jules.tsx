eedcc7e6f9f77942b55112c1c942f99260d1429a
Luis fernando JU <luigfmp@gmail.com>
2025-01-15 22:05:48 -0500

    refactor: :recycle: refactor code

--- src/shared/assets/Jules.tsx
+++ src/shared/assets/Jules.tsx
@@ -2,71 +2,166 @@ import type { JSX } from 'react'
 
 const Jules = (): JSX.Element => {
   return (
-    <svg viewBox='0 0 58 67' fill='none'>
-      <g filter='url(#filter0_d_24_66)'>
-        <path
-          d='M54 33.1081C54 46.9152 42.8071 58.1081 29 58.1081C15.1929 58.1081 4 46.9152 4 33.1081C4 19.301 15.1929 8.10811 29 8.10811C42.8071 8.10811 54 19.301 54 33.1081Z'
-          fill='#E8EEFF'
-        />
-        <path
-          d='M19.097 54.7845C30.1001 46.2207 36.6811 46.7671 48.3878 54.2413C41.9291 39.5182 45.1024 34.0178 54 27.5475C32.5923 30.9311 23.5546 36.1733 19.097 54.7845Z'
-          fill='url(#paint0_linear_24_66)'
-        />
-        <path
-          d='M17.3287 52.4566C20.9935 30.9409 15.0224 27.5475 13.7154 20.0204C12.4085 12.4934 16.4212 8.19963 18.6357 6.67347C20.8502 5.14731 33.328 2.83986 35.0109 0L46.9271 1.24158C37.4133 4.03515 34.6062 6.36814 31.3207 12.4158C29.9226 16.424 29.3011 19.9112 28.2455 26.5387C25.5549 36.7816 22.9989 42.4431 17.3287 52.4566Z'
-          fill='url(#paint1_linear_24_66)'
-        />
-      </g>
+    <svg
+      width='151'
+      height='150'
+      viewBox='0 0 151 150'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M119.744 97.4359C132.95 90.1523 137.77 84.0815 133.974 69.359C131.133 69.251 129.875 67.2473 129.231 64.2307C129.347 60.8664 125.962 56.4102 124.359 55.2564C122.358 53.816 120.385 53.5897 117.82 53.5897C117.82 53.5897 108.077 56.4102 99.6154 56.4102C105.224 61.7697 108.453 73.6157 105.128 83.8461L119.744 97.4359Z'
+        fill='url(#paint0_linear_37_143)'
+      />
+      <path
+        d='M75.8506 150C117.272 150 150.851 116.421 150.851 75C150.851 61.9957 147.541 49.7644 141.718 39.1025C143.598 58.2702 141.962 65.1929 133.974 69.359C137.77 84.0815 132.95 90.1523 119.744 97.4359C120.06 116.21 109.08 137.859 75.8506 150Z'
+        fill='url(#paint1_diamond_37_143)'
+      />
+      <path
+        d='M133.974 69.359C141.962 65.193 143.598 58.2702 141.718 39.1026C134.639 26.1413 123.846 15.4993 110.769 8.60754C102.311 38.3736 103.177 48.3005 115.128 48.3333C123.473 48.5173 128.027 49.7777 135.513 57.9487C137.266 60.2816 136.972 62.6224 133.974 69.359Z'
+        fill='url(#paint2_linear_37_143)'
+      />
+      <path
+        d='M129.231 64.2307C129.875 67.2472 131.133 69.2509 133.974 69.3589C136.972 62.6223 137.266 60.2816 135.513 57.9487C128.027 49.7776 123.473 48.5172 115.128 48.3333C103.177 48.3005 102.311 38.3735 110.769 8.60749C94.533 2.95133 86.3363 1.15618 75.8506 4.23071C89.8726 16.189 86.3395 25.7072 75.8506 43.7179C86.8491 44.8803 92.319 47.4196 99.6154 56.4102C108.077 56.4102 117.821 53.5897 117.821 53.5897C120.385 53.5897 122.358 53.816 124.359 55.2563C125.962 56.4102 129.347 60.8664 129.231 64.2307Z'
+        fill='url(#paint3_linear_37_143)'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M10.5267 76.2821C16.8102 63.5261 23.9602 54.1753 32.389 47.8735C31.1183 68.6108 33.4738 77.9352 48.4616 88.5898C26.6333 91.4518 19.8427 87.6871 10.5267 76.2821Z'
+        fill='url(#paint4_linear_37_143)'
+      />
+      <path
+        d='M75.8506 150C63.7993 143.718 57.6454 135.385 57.6454 131.154C57.8974 127.666 60.445 117.035 78.4147 106.41C55.1121 119.519 42.0044 119.359 35.7224 114.359C28.9275 117.051 17.7554 114.517 5.46594 100.963C16.0287 129.589 43.556 150 75.8506 150Z'
+        fill='url(#paint5_linear_37_143)'
+      />
+      <path
+        d='M0 75.1282C0 81.591 1.66799 87.7347 3.20513 93.7179C5.51888 87.3169 7.94993 81.5131 10.5267 76.282C16.8101 63.526 23.9601 54.1752 32.389 47.8734C39.8086 42.3263 48.2191 39.1417 57.9018 38.0769C45.9966 35.5147 39.0984 35.4123 30.8505 39.1025C42.3119 13.0554 53.5479 7.68358 67.5172 4.23071C45.7074 5.22598 33.405 10.9487 11.2352 36.8997C4.63691 48.0658 0 61.2189 0 75.1282Z'
+        fill='url(#paint6_linear_37_143)'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M57.9018 38.0769C45.9966 35.5147 39.0984 35.4123 30.8505 39.1026C42.3119 13.0555 53.5479 7.68364 67.5172 4.23077C45.7074 5.22604 33.405 10.9488 11.2352 36.8998C24.2863 14.8136 48.3385 0 75.8505 0C88.4587 0 100.34 3.11112 110.769 8.60755C94.533 2.95139 86.3363 1.15623 75.8505 4.23077C89.8726 16.1891 86.3394 25.7072 75.8505 43.718C86.849 44.8804 92.319 47.4196 99.6154 56.4103C105.224 61.7697 108.453 73.6157 105.128 83.8462L119.744 97.4359C120.06 116.21 109.08 137.859 75.8505 150C63.7993 143.718 57.6454 135.385 57.6454 131.154C57.8974 127.666 60.4449 117.035 78.4146 106.41C55.1121 119.519 42.0044 119.359 35.7223 114.359C28.9275 117.051 17.7553 114.517 5.46593 100.963C4.59461 98.6014 3.83873 96.1842 3.20513 93.718C5.51888 87.317 7.94993 81.5132 10.5267 76.2821C19.8427 87.687 26.6333 91.4518 48.4615 88.5898C33.4737 77.9351 31.1183 68.6108 32.389 47.8735C39.8086 42.3263 48.2191 39.1417 57.9018 38.0769ZM75.1282 105C91.5551 105 104.872 91.6833 104.872 75.2564C104.872 58.8295 91.5551 45.5128 75.1282 45.5128C58.7013 45.5128 45.3846 58.8295 45.3846 75.2564C45.3846 91.6833 58.7013 105 75.1282 105Z'
+        fill='url(#paint7_radial_37_143)'
+      />
+      <ellipse
+        cx='75.3846'
+        cy='74.9999'
+        rx='20.7692'
+        ry='20.7692'
+        fill='url(#paint8_radial_37_143)'
+      />
       <defs>
-        <filter
-          id='filter0_d_24_66'
-          x='0'
-          y='0'
-          width='58'
-          height='66.1082'
-          filterUnits='userSpaceOnUse'
-          colorInterpolationFilters='sRGB'
-        >
-          <feFlood floodOpacity='0' result='BackgroundImageFix' />
-          <feColorMatrix
-            in='SourceAlpha'
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
-            result='hardAlpha'
-          />
-          <feOffset dy='4' />
-          <feGaussianBlur stdDeviation='2' />
-          <feComposite in2='hardAlpha' operator='out' />
-          <feColorMatrix type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0' />
-          <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_24_66' />
-          <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_24_66' result='shape' />
-        </filter>
         <linearGradient
-          id='paint0_linear_24_66'
-          x1='35.6667'
-          y1='17.9167'
-          x2='16.0286'
-          y2='42.3827'
+          id='paint0_linear_37_143'
+          x1='137.564'
+          y1='65.8974'
+          x2='110.811'
+          y2='61.1755'
           gradientUnits='userSpaceOnUse'
         >
-          <stop stopColor='#32AAFF' />
-          <stop offset='0.431417' stopColor='#2F40FF' />
-          <stop offset='0.771417' stopColor='#201A6B' />
-          <stop offset='1' stopColor='#0D0B2E' />
+          <stop offset='0.395' stopColor='#7D2190' />
+          <stop offset='1' stopColor='#F79AFF' />
         </linearGradient>
+        <radialGradient
+          id='paint1_diamond_37_143'
+          cx='0'
+          cy='0'
+          r='1'
+          gradientUnits='userSpaceOnUse'
+          gradientTransform='translate(146.41 56.4102) rotate(124.706) scale(128.348 79.6182)'
+        >
+          <stop offset='0.316907' stopColor='#2D2867' />
+          <stop offset='0.443442' stopColor='#7C218F' />
+          <stop offset='0.651414' stopColor='#F582FF' />
+          <stop offset='0.790683' stopColor='#A444B3' />
+          <stop offset='0.952333' stopColor='#2D2867' />
+        </radialGradient>
         <linearGradient
-          id='paint1_linear_24_66'
-          x1='35.6667'
-          y1='17.9167'
-          x2='16.0286'
-          y2='42.3827'
+          id='paint2_linear_37_143'
+          x1='142.308'
+          y1='31.4102'
+          x2='110.327'
+          y2='47.9176'
           gradientUnits='userSpaceOnUse'
         >
-          <stop stopColor='#32AAFF' />
-          <stop offset='0.431417' stopColor='#2F40FF' />
-          <stop offset='0.771417' stopColor='#201A6B' />
-          <stop offset='1' stopColor='#0D0B2E' />
+          <stop stopColor='#23245E' />
+          <stop offset='0.395' stopColor='#7D2190' />
+          <stop offset='1' stopColor='#F79AFF' />
         </linearGradient>
+        <linearGradient
+          id='paint3_linear_37_143'
+          x1='131.795'
+          y1='25.5128'
+          x2='82.1795'
+          y2='42.8205'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop offset='0.0423334' stopColor='#1D2159' />
+          <stop offset='0.582333' stopColor='#7D2190' />
+          <stop offset='1' stopColor='#F79AFF' />
+        </linearGradient>
+        <linearGradient
+          id='paint4_linear_37_143'
+          x1='15'
+          y1='63.3334'
+          x2='46.9231'
+          y2='87.9488'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#111335' />
+          <stop offset='0.473675' stopColor='#A837BD' />
+          <stop offset='1' stopColor='#EA83FF' />
+        </linearGradient>
+        <linearGradient
+          id='paint5_linear_37_143'
+          x1='33.718'
+          y1='139.359'
+          x2='59.2487'
+          y2='105.578'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#141D51' />
+          <stop offset='0.368813' stopColor='#7D2190' />
+          <stop offset='1' stopColor='#F79AFF' />
+        </linearGradient>
+        <linearGradient
+          id='paint6_linear_37_143'
+          x1='-1.92308'
+          y1='65.8974'
+          x2='62.6923'
+          y2='11.923'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#141D51' />
+          <stop offset='0.315' stopColor='#7D2190' />
+          <stop offset='1' stopColor='#F79AFF' />
+        </linearGradient>
+        <radialGradient
+          id='paint7_radial_37_143'
+          cx='0'
+          cy='0'
+          r='1'
+          gradientUnits='userSpaceOnUse'
+          gradientTransform='translate(75.4253 75) rotate(90) scale(75 75.4253)'
+        >
+          <stop offset='0.128291' stopColor='#FAC7FF' />
+          <stop offset='0.201704' stopColor='#D670FF' />
+          <stop offset='0.847743' stopColor='#141D51' />
+        </radialGradient>
+        <radialGradient
+          id='paint8_radial_37_143'
+          cx='0'
+          cy='0'
+          r='1'
+          gradientUnits='userSpaceOnUse'
+          gradientTransform='translate(76.923 74.9999) rotate(-13.7531) scale(37.749 36.5173)'
+        >
+          <stop offset='0.242333' stopColor='#F485FE' />
+          <stop offset='0.567333' stopColor='#5A207B' />
+          <stop offset='0.897333' stopColor='#141D51' />
+        </radialGradient>
       </defs>
     </svg>
   )
